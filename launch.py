078208ed564f32d5f98292ca871cf96279c526ed
Yuri Numerov <achi@hush.ai>
2014-10-25 05:49:54 +0200

    menu working

--- launch.py
+++ launch.py
@@ -8,23 +8,58 @@ def tkexit():
   """
   exit()
 
+def clsr():
+  """
+  Deletes all the elements in the screen 
+  """
+  global elements
+  for i in elements: i.pack_forget()
+  elements=[]
+
 def mmenu():
   """
   main menu
   """
-  results=Tkinter.Label(text="\nKUROBOT\nv0.0.0\n\n\n\n")
-  #results.grid(row=1, column=1)
-  results.pack()
+
+  clsr()
+  title=Tkinter.Label(text="\nKUROBOT\nv0.0.0\n\n\n\n")
+  title.pack()
+  elements.append(title)
   opt1button=Tkinter.Button(text="New Game",width=10)
   opt1button.pack()
+  elements.append(opt1button)
   opt2button=Tkinter.Button(text="Continue",width=10)
   opt2button.pack()
-  opt3button=Tkinter.Button(text="Options",width=10)
+  elements.append(opt2button)
+  opt3button=Tkinter.Button(text="Options",width=10,command=optionm)
   opt3button.pack()
+  elements.append(opt3button)
   opt4button=Tkinter.Button(text="Exit",width=10,command=tkexit)
   opt4button.pack()
-    
+  elements.append(opt4button)
+
+def optionm():
+  """
+  Options menu
+  """
+
+  clsr()
+  title=Tkinter.Label(text="\nKUROBOT\nv0.0.0\n\n\n\n")
+  title.pack()
+  elements.append(title)
+  opt1button=Tkinter.Button(text="Option1",width=10)
+  opt1button.pack()
+  elements.append(opt1button)
+  opt2button=Tkinter.Button(text="Option2",width=10)
+  opt2button.pack()
+  elements.append(opt2button)
+  opt3button=Tkinter.Button(text="Back",width=10,command=mmenu)
+  opt3button.pack()
+  elements.append(opt3button)
+
+
 if __name__=="__main__":
+  elements=[] 
   root = Tkinter.Tk()
   root.wm_title("Kurobot dev")
   mmenu()

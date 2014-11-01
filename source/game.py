42f6698b60105c518c7492df9c8a09f09fe35e55
Yuri Numerov <achi@hush.ai>
2014-11-01 05:44:53 +0100

    updated menus

--- source/game.py
+++ source/game.py
@@ -28,10 +28,44 @@ def makewin():
   gamemenu=Tkinter.Menu(menubar, tearoff=0)
   gamemenu.add_command(label="Load")
   gamemenu.add_command(label="Save")
+  gamemenu.add_command(label="Options")
   gamemenu.add_separator()
-  gamemenu.add_command(label="Exit")
+  gamemenu.add_command(label="Exit",command=exitwin)
+  #Team management drop down menu
+  teammenu=Tkinter.Menu(menubar, tearoff=0)
+  teammenu.add_command(label="Manage")
+  teammenu.add_command(label="Members")
+  teammenu.add_command(label="Train")
+  teammenu.add_command(label="Info")
+  #Robot management drop down menu
+  robotmenu=Tkinter.Menu(menubar, tearoff=0)
+  robotmenu.add_command(label="Status")
+  robotmenu.add_command(label="components")
+  robotmenu.add_command(label="Upgrades")
+  robotmenu.add_command(label="Repair")
+  #Finances drop down menu
+  financesmenu=Tkinter.Menu(menubar, tearoff=0)
+  financesmenu.add_command(label="Bank")
+  financesmenu.add_command(label="Sponsors")
+  financesmenu.add_command(label="Expenses")
+  #Menu integration
   menubar.add_cascade(menu=gamemenu, label="Game")
+  menubar.add_cascade(menu=teammenu, label="Team")
+  menubar.add_cascade(menu=robotmenu, label="Robot")
+  menubar.add_cascade(menu=financesmenu, label="Finances")
   print "OK"
   Tkinter.mainloop()
 
-if __name__=="__main__": pass
+def exitwin():
+  """
+  Closes the game window and returns to the main menu
+  """
+
+  pass
+
+if __name__=="__main__": 
+
+  print "Game window and main procedures module"
+  print "Testing... ",
+  print "[disabled]"
+  exit()

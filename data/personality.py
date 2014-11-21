0292abe4fc617937b37cc81e3e0a89de729242f3
Yuri Numerov <achi@hush.ai>
2014-11-21 18:12:39 +0100

    added data files

---
+++ data/personality.py
@@ -0,0 +1,20 @@
+traits={
+
+  "physical":{
+    "overall":          #00: Quatto,          99: Apollo
+  }
+  "mental":{
+    "Concentration":  #00: Hyperactive,     99: Utterly absorbed
+    "Discipline":     #00: Lazy bastard,    99: Ninja discipline
+    "Memory":         #00: Memento guy,     99: living git repo
+    "Creativity":     #00: Bureaucrat,      99: god on LSD
+    "Mental":         #00: Cthulhu victim,  99: 
+
+  }
+  "social":{
+    "overall":          #00: Forest hermit,   99: Master social engineer
+    "Patience":         #00: 6 year old,      99: Buddha
+    "Tolerance":        #00: KKK leader,      99: $4 chinese lock
+  }
+
+}
---
+++ data/skills.py
@@ -0,0 +1,79 @@
+skills={
+
+  "computers":{
+
+    "general":
+    "hardware":
+    "programming":
+    "security":
+    "software":
+  }
+
+  "misc-engineering":{
+
+    "architecture":
+    "electrical":
+    "electronics":
+    "materials":
+    "telecom":
+  }
+
+  "hardware":{
+
+    "security":
+    "welding":
+    "woodworking":
+  }
+
+  "hobbies":{
+
+    "knitting":
+    "writing":
+  }
+
+  "mechanical":{
+
+    "design":
+    "general":
+    "hydraulics":
+    "pneumatics":
+    "repairs":
+  }
+
+  "misc-skills":{
+
+    "bureaucracy":
+    "languages":
+    "management":
+    "social skills":
+    "strategy":
+  }
+
+  "outdoors":{
+
+    "first aid":
+    "orienteering":
+    "survival":
+    "training":
+  }
+
+  "sciences":{
+
+    "biology":
+    "chemistry":
+    "mathematics":
+    "medicine":
+    "physics":
+  }
+
+  "social-sci":{
+
+    "economics":
+    "geography":
+    "history":
+    "law":
+    "politics":
+    "psichology":
+  }
+  
+  }
